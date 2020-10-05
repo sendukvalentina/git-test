0d1be5b384cb45a50c6ff83ab9470b9afcd314d9
sendukvalentina <sendukvalentina@gmail.com>
2020-10-05 10:32:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
 <h1>Hello world</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quas optio provident!</p>
+<ul>
+    <li>satu </li>
+    <li>dua </li>
+</ul>
 </body>
 </html>
